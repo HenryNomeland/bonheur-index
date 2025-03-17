7b5ad385e0544cbb442f950a45a5ce5220e7a776
HenryNomeland <henrynomeland@gmail.com>
2025-03-17 09:06:26 -0500

    image and reorg updates

--- base_classes.py
+++ base_classes.py
@@ -2,57 +2,63 @@ import pandas as pd
 from datetime import datetime
 from abc import ABC, abstractmethod
 import dateparser
+import re
 
 
 class ArtReview:
     def __init__(
         self,
-        title="Untitled Review",
-        reviewer="Unknown Reviewer",
-        date="Unknown Date",
-        content="Inaccessible Content",
-        artist="Unknown Artist",
-        artwork="Unknown Artwork",
-        location="Unknown Location",
-        city="Unknown City",
-        url="",
+        title="NA",
+        reviewer="NA",
+        date="NA",
+        content="NA",
+        artist="NA",
+        artwork="NA",
+        location="NA",
+        city="NA",
+        img_path="NA",
+        url="NA",
     ):
-        self.title = self.format_title(title)
+        self.title = title
         self.reviewer = reviewer
-        self.date = self.format_date(date)
-        self.content = self.format_content(content)
+        self.date = date
+        self.content = content
         self.artist = artist
         self.artwork = artwork
         self.location = location
         self.city = city
+        self.img_path = img_path
         self.url = url
 
     def format_title(self, title):
-        return title.strip().title() if title else "Untitled Review"
+        return title.strip().title() if title != "NA" else "NA"
 
     def format_date(self, date):
         try:
-            print(date)
             parsed_date = dateparser.parse(
-                date, settings={"PREFER_DATES_OF_MONTH": "first"}
+                date, settings={"PREFER_DAY_OF_MONTH": "first"}
             )
             return parsed_date.strftime("%Y-%m-%d")
         except ValueError:
-            return "Unknown Date"
+            return "NA"
+
+    def format_reviewer(self, reviewer):
+        return re.sub(r"^by\s+", "", reviewer, flags=re.IGNORECASE).strip()
 
     def format_content(self, content):
-        return " ".join(content.split()) if content else ""
+        return " ".join(content.split())
 
     def to_dict(self):
         return {
-            "Title": self.title,
-            "Reviewer": self.reviewer,
-            "Date": self.date,
-            "Content": self.content,
+            "Title": self.format_title(self.title),
+            "Reviewer": self.format_reviewer(self.reviewer),
+            "Date": self.format_date(self.date),
+            "Content": self.format_content(self.content),
             "Artist": self.artist,
             "Artwork": self.artwork,
             "Location": self.location,
             "City": self.city,
+            "Image": self.img_path,
             "URL": self.url,
         }
 
--- scrapers.py
+++ scrapers.py
@@ -2,50 +2,90 @@ from base_classes import ArtReview, ArtScraper
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import os
+import string
+import traceback
 
 
 class ArtForumScraper(ArtScraper):
+    def _get_content_div(self, title_url):
+        content_response = requests.get(title_url)
+        if content_response.status_code == 200:
+            content_soup = BeautifulSoup(content_response.text, "html.parser")
+            content_div = content_soup.find("div", attrs={"class": "_main_10r4d_1"})
+            if content_div:
+                return content_div
+            else:
+                return "NA"
+        else:
+            return "NA"
+
+    def _get_review_attributes(self, card, date):
+        title_div = card.find("div", attrs={"data-alias": "card__card-title"})
+        review = ArtReview(url=title_div.find("a")["href"], date=date)
+        title_text = title_div.get_text()
+        if title_text[0] == "“":
+            review.artwork = title_text[1:-1]
+            review.artist = "NA"
+        else:
+            review.artist = title_text
+            review.artwork = "NA"
+        div = self._get_content_div(review.url)
+        if div == "NA":
+            return review
+        para_list = []
+        for para in div.find_all("p"):
+            para_list.append(para.get_text())
+        review.content = " ".join(para_list)
+        review.city = div.find(
+            "div", attrs={"data-alias": "article-header__location"}
+        ).get_text(strip=True)
+        locations = div.find_all("div", attrs={"data-alias": "article-header__venue"})
+        location_list = []
+        for location in locations:
+            location_list.append(location.get_text(strip=True))
+        review.location = ", ".join(location_list)
+        review.reviewer = div.find("div", attrs={"data-theme": "byline-lg"}).get_text()
+        img_tag = div.find("img")
+        img_link = img_tag["src"]
+        img_name = img_tag["alt"].rstrip(string.punctuation)
+        review.img_path = os.path.join(
+            ".", "images", "ArtForum", review.date, f"{img_name}.jpg"
+        )
+        if not os.path.exists(review.img_path):
+            try:
+                os.makedirs(os.path.dirname(review.img_path), exist_ok=True)
+                response = requests.get(img_link, stream=True)
+                response.raise_for_status()
+                with open(review.img_path, "wb") as f:
+                    for chunk in response.iter_content(1024):
+                        f.write(chunk)
+                print(f"Image Downloaded - {img_name}")
+            except Exception as e:
+                print(f"Image download failed - {img_name}")
+                print(e)
+        else:
+            print(f"Already Downloaded - {img_name}")
+        return review
+
     def scrape(self):
         response = requests.get(self.url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, "html.parser")
-            promotion_widget = soup.find(
+            cards = soup.find_all("div", attrs={"data-alias": "card__inner"})
+            promo_widget = soup.find(
                 "div", attrs={"data-component": "promotion-widget"}
             )
-            if promotion_widget:
-                date_text = promotion_widget.find(
+            if promo_widget:
+                date = promo_widget.find(
                     "div", attrs={"data-alias": "promotion__primary-text"}
                 ).get_text(strip=True)
-            cards = soup.find_all("div", attrs={"data-alias": "card__inner"})
             reviews = []
             for card in cards:
-                print(card)
                 try:
-                    city_text = card.find(
-                        "div", attrs={"data-alias": "card__location"}
-                    ).get_text(strip=True)
-                    title_div = card.find(
-                        "div", attrs={"data-alias": "card__card-title"}
-                    )
-                    title_text = title_div.get_text()
-                    if title_text[0] == "“":
-                        artwork_text = title_text[1:-1]
-                        artist_text = "Unknown Artist"
-                    else:
-                        artist_text = title_text
-                        artwork_text = "Unknown Artwork"
-                    title_url = title_div.find("a")["href"]
-                    review = ArtReview(
-                        date=date_text,
-                        artist=artist_text,
-                        artwork=artwork_text,
-                        city=city_text,
-                        url=title_url,
-                    )
+                    review = self._get_review_attributes(card, date)
                     reviews.append(review.to_dict())
-                    print(f"No Attribute Error - {card}")
-                except AttributeError:
-                    print(f"Attribute Error - {card}")
+                except Exception as e:
                     continue
             self.data = pd.DataFrame(reviews)
         else:
